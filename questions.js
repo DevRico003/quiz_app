dfb24202bc35bb6f1bf192715f2ffa0f8d4ad530
Enrico Carteciano <carteciano.enrico@gmail.com>
2023-09-27 11:11:31 +0200

    first commit

---
+++ questions.js
@@ -0,0 +1,105 @@
+   //Questions array
+            let questions = [
+            {
+                 number:1,
+                 question: "Was ist die Hauptstadt von Deutschland?",
+                 answer : "Berlin",
+                 options : [
+                                  "Berlin",
+                                  "München",
+                                  "Hamburg",
+                                  "Frankfurt"
+                                ]
+            
+            },
+            {
+                 number:2,
+                 question: "Welches Tier gibt uns Milch?",
+                 answer : "Kuh",
+                 options : [
+                                  "Huhn",
+                                  "Schwein",
+                                  "Pferd",
+                                  "Kuh"  
+                                  ]
+            
+            },
+           {
+                 number:3,
+                 question: "What does URL stand for ?",
+                 answer : "Uniform Resource Locator",
+                 options : [ 
+                                  "Universal Resource Language",
+                                  "Uniform Resource Locator",
+                                  "Universal Resource Locator",
+                                  "University Resource Link "
+                                  ]
+            
+            },
+            {
+                 number:4,
+                 question: "What does HTTP stand for ?",
+                 answer : "Hypertext Transfer Protocol",
+                 options : [
+                                  "Hypertext Transmitter Protocol",
+                                  "Hypertest Transfer Protocol",
+                                  "Hypertech Transit Protocol",
+                                  "Hypertext Transfer Protocol"
+                                  ]
+            }, 
+            {
+                 number:5,
+                 question: "What does SEO stand for ?",
+                 answer : "Search Engine Optimization",
+                 options : [ 
+                                  "Search Engine Orientation",
+                                  "Script Engine Optimization",
+                                  "Script Entension Order",
+                                  "Search Engine Optimization"
+                               ]
+            },
+            {
+                 number:6,
+                 question: "What does API stand for ?",
+                 answer : "Application Programming Interface",
+                 options : [
+                                  "Application Programming Interface",
+                                  "Appliances Programming Interface",
+                                  "Application Programming Instruction",
+                                  "Appliances Programming Instruction"
+                               ]
+            },
+            {
+                 number:7,
+                 question: "What does CSS stand for ?",
+                 answer : "Cascading Style Sheet",
+                 options : [
+                                  "Colorful Style Sheet",
+                                  "Cascading Sheet Style",
+                                  "Cascading Style Sheet",
+                                  "Creative Style Sheet"
+                               ]
+            },
+             {
+                 number:8,
+                 question: "What does DOM stand for ?",
+                 answer : "Document Object Model",
+                 options : [
+                                 "Document Object Model",
+                                 "Docstring Object Model",
+                                 "Doctype Object Model",
+                                 "Document Object Module"
+                               ]
+            },
+            {
+                 number:9,
+                 question: "What does CMS stand for ?",
+                 answer : "Content Management System",
+                 options : [
+                                 "Content Managent System",
+                                 "Content Manager System",
+                                 "Content Managing System",
+                                 "Content Management System"
+                               ]
+            }
+             ];    
